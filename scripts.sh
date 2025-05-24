c0343518443398e0998f84bdf79caa6253d3697a
ring1012 <tanghuan93@foxmail.com>
2025-05-24 14:50:04 +0800

    Create scripts.sh

---
+++ scripts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker stop r1
+docker rm r1
+docker run  --pull=always  --restart=always --name="r1" -it -d --network=host -e password=123456  -v /root/r1-iot/:/root/.r1-iot registry.cn-hangzhou.aliyuncs.com/ring1012/r1
